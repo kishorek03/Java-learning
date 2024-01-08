ce7791a9f58a14d0e44547098ee4ae4fdb905bcc
Kishore K <kishorekarthik2003@gmail.com>
2024-01-08 13:49:12 +0530

    added

---
+++ src/SunnyNumber.java
@@ -0,0 +1,18 @@
+public class SunnyNumber {
+    public static void main(String[] args) {
+        int [] inputNumbers = ScannerHelper.getInputInts();
+        for(int input :inputNumbers){
+            sunnyNumber(input);
+        }
+    }
+
+    public static void sunnyNumber(int inputNumber) {
+        double n = inputNumber+1;
+        if(Math.sqrt(n)%1==0){
+            System.out.println("The given number is a sunny number");}
+        else{
+            System.out.println("The given number is not a prime number");
+        }
+
+    }
+}
