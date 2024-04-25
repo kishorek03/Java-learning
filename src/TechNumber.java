5dc263b4ccc7ff7766fcdbe9e45af7798d8d37e8
Kishore K <kishorekarthik2003@gmail.com>
2024-04-25 14:48:05 +0530

    commiting new file

---
+++ src/TechNumber.java
@@ -0,0 +1,22 @@
+public class TechNumber {
+    public static void main(String[] args) {
+        int [] inputNumbers = ScannerHelper.getInputInts();
+        for(int input:inputNumbers){
+            toFindTechNumber(input);
+        }
+    }
+
+    public static void toFindTechNumber(int input) {
+        int count=0;
+        while(input>0){
+            count ++;
+            input/=10;
+        }
+        //if (input%2==0){
+
+       // }
+
+    }
+}
+
+
